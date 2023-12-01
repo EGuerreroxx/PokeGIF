776a6d100ac78a787a21fde2fff99ad59fe9cee1
Lycan <guerrerodelanoche300@gmail.com>
2023-11-30 21:33:36 -0500

    added giphy class to display the url from the giphy API, cleaned up front page and commnented out Poke API

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,31 @@
+var giphyDisplay = document.querySelector(".giphy");
+
+
+
 function getGifData(search) {
     fetch('http://api.giphy.com/v1/gifs/search?api_key=qV1xiffn8m3Ccvrhl7MV4hp9WtzV6bsq&limit=20&q=Pokemon')
-        .then(function(response) {
-        return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-        })
-    }
-    getGifData();
-  
-
-    function getPokeData(description) {
-        fetch('https://pokeapi.co/api/v2/pokemon/pikachu/?limit=20&offset=20')
-            .then(function(response) {
+        .then(function (response) {
             return response.json();
-            })
-            .then(function(data) {
-                console.log(data);
-            })
-        }
-     getPokeData();
+        })
+        .then(function (data) {
+            console.log(data.data);
+            for (var i = 0; i < data.data.length; i++) {
+                var display = document.createElement('p');
+                display.textContent = data.data[i].url;
+                giphyDisplay.append(display);
+            }
+        })
+}
+getGifData();
+
+
+// function getPokeData(description) {
+//     fetch('https://pokeapi.co/api/v2/pokemon/pikachu/?limit=20&offset=20')
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+// }
+// getPokeData();
