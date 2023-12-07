6a05a1b890bbd6e0b79c589dece9c5fd09d85301
Lycan <guerrerodelanoche300@gmail.com>
2023-12-06 21:20:04 -0500

    Added LIVE URL and Screenshots  to README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# PokeGIF
+# pokeGIF
+
+## Description
+Our webpage has been built from the ground the up, combining two existing API (Giphy & Poke),
+to bring forth useful data and pokemon gifs to the user on one convient platform. The purpose of
+our webpage is to have users quick search their interested pokemon, and have the base stats of
+them pop up in an instant to have ready for usage, and following along with it an assortment of
+related gifs to enjoy and share. The search feature has been designed to speed track the most asked
+information about a pokemon's attributes, it's base stats. Users will be engaged in continous searches
+back to back with relevant, useful, and enjoyable data on every search, covering various pokemon they
+wish to search without scrolling and passing through tedious pages. In all-in-one utility for every
+pokemon fan of any knowledge experience. __Technology__ used is bootswatch CSS framework, theme
+sketchy. The pokemon stats were pulled using Poke API and the gifs related to the pokemon were
+generated using giphy API. 
+
+## Usage
+Users will input in the search box the name of the pokemon they wish to look up, and then click the 
+search button. The base stats will appear first on info cards denoting the stat name and stat number 
+i.e( Attack - 125). Follwing the info cards there will be an assortment of gifs related to the pokemon
+searched.
+
+[Live URL](https://eguerreroxx.github.io/PokeGIF/)
+
+## Screenshot
+![Screenshot of PokeGIF](./screenshots/Default_Page.jpg)
+![Screenshot of PokeGIF](./screenshots/Pokemon_Searched_Page.jpg)
+
--- assets/script.js
+++ assets/script.js
@@ -2,71 +2,80 @@ var giphyDisplay = document.querySelector("#giphy");
 var pokeData = document.querySelector("#poke-data");
 var btn = document.getElementById("btn");
 var searchInput = document.querySelector("#search");
-
+var listContainer = document.querySelector("#list-of-pokemon");
+function clearContent() {
+    giphyDisplay.innerHTML = '';
+    pokeData.innerHTML = '';
+}
 function getGifData(search) {
-    fetch('http://api.giphy.com/v1/gifs/search?api_key=qV1xiffn8m3Ccvrhl7MV4hp9WtzV6bsq&limit=20&q=Pokemon ' + search)
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=qV1xiffn8m3Ccvrhl7MV4hp9WtzV6bsq&limit=20&q=Pokemon ' + search)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data.data);
-
+            localStorage.setItem('giphyData', JSON.stringify(data.data));
             for (var i = 0; i < data.data.length; i++) {
                 var display = document.createElement('img');
-                display.src = data.data[i].images["480w_still"].url;
+                display.src = data.data[i].images.preview_gif.url;
                 giphyDisplay.append(display);
             }
         })
 }
-
-
 function getPokeData(value) {
     var search = value.toLowerCase();
-    fetch('https://pokeapi.co/api/v2/pokemon/' + search + '/?limit=20&offset=20')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            for (var i = 0; i < data.moves.length; i++) {
-                //     <article class="card text-white bg-info mb-3">
-                //     <header class="card-header">Pokemon Stats:</header>
-                //     <section class="card-body">
-                //       <h4 class="card-title">Pokemon Name</h4>
-                //       <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio veniam asperiores perspiciatis dolorum incidunt a pariatur ex sapiente vitae in temporibus molestias minus, alias quo ea quod vel tenetur quaerat?</p>
-                //       </section>
-                //   </article>
-                var colEl = document.createElement('div');
-                colEl.classList.add('col', 'col-md-4');
-                var cardEl = document.createElement('article');
-                cardEl.classList.add('card', 'text-white', 'bg-info', 'mb-3');
-                var headerEl = document.createElement('header');
-                headerEl.classList.add('card-header');
-                headerEl.textContent = 'Pokemon Stats:';
-                var sectionEl = document.createElement('section');
-                sectionEl.classList.add('card-body');
-                var h4El = document.createElement('h4');
-                h4El.classList.add('card-title');
-                h4El.textContent = data.moves[i].move.name;
-                var pEl = document.createElement('p');
-                pEl.classList.add('card-text');
-                pokeData.append(colEl);
-                colEl.append(cardEl);
-                cardEl.append(headerEl, sectionEl);
-                sectionEl.append(h4El, pEl);
-            }
-            getGifData(search);
-        })
+    clearContent();
+    var storedPokeData = localStorage.getItem(search);
+    if (storedPokeData) {
+        displayPokeData(JSON.parse(storedPokeData), search);
+    } else {
+        fetch('https://pokeapi.co/api/v2/pokemon/' + search + '/?limit=20&offset=20')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                localStorage.setItem(search, JSON.stringify(data));
+                displayPokeData(data, search);
+            });
+    }
+}
+function displayPokeData(data, search) {
+    for (var i = 0; i < data.stats.length; i++) {
+        var colEl = document.createElement('div');
+        colEl.classList.add('col', 'col-md-4');
+        var cardEl = document.createElement('article');
+        cardEl.classList.add('card', 'text-white', 'bg-info', 'mb-3');
+        var headerEl = document.createElement('header');
+        headerEl.classList.add('card-header');
+        headerEl.textContent = 'Pokemon Stats:';
+        var sectionEl = document.createElement('section');
+        sectionEl.classList.add('card-body');
+        var h4El = document.createElement('h4');
+        h4El.classList.add('card-title');
+        h4El.textContent = data.stats[i].stat.name.toUpperCase() + [" "] + data.stats[i].base_stat;
+        var pEl = document.createElement('p');
+        pEl.classList.add('card-text');
+        pokeData.append(colEl);
+        colEl.append(cardEl);
+        cardEl.append(headerEl, sectionEl);
+        sectionEl.append(h4El, pEl);
+    }
+    getGifData(search);
 }
 
-
-btn.addEventListener('keypress', function (event) {
+btn.addEventListener('click', function (event) {
     event.preventDefault();
-    if (e.key === 'Enter') {
-        getPokeData(searchInput.value);
-      }
-    
+    getPokeData(searchInput.value);
 });
+
+
+// btn.addEventListener('keypress', function (event) {
+//     event.preventDefault();
+//     if(event.key === 'Search'){
+//     getPokeData(searchInput.value);
+//     }
+// });
 
 // document.querySelector('#txtSearch').addEventListener('keypress', function (e) {
 //     if (e.key === 'Enter') {
